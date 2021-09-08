9166605d394c7faa2c101db9a3f649ef3bdc341b
jianxhee <jianxhee@gmail.com>
2021-09-09 01:17:36 +0800

    updated prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -2,20 +2,17 @@ from azure.cognitiveservices.vision.customvision.prediction import CustomVisionP
 from msrest.authentication import ApiKeyCredentials
 import matplotlib.pyplot as plt
 from numpy import round
-from PIL import Image
-import os
+
 
 def predict(image_path):
     project_id = '4d2f151e-7d72-4cb3-8512-9a3a7e43f26e'
     cv_key = 'e9b017ef95734a6ebef8ab317607b19c'
     cv_endpoint = 'https://southeastasia.api.cognitive.microsoft.com/'
 
-    model_name = 'RiceDiseaseClassifier' # this must match the model name you set when publishing your model iteration (it's case-sensitive)!
+    model_name = 'RiceDiseaseClassifier'  # this must match the model name you set when publishing your model iteration (it's case-sensitive)!
     print('Ready to predict using model {} in project {}'.format(model_name, project_id))
 
-
     # Get the test images from the data/vision/test folder
-    
 
     # Create an instance of the prediction service
     credentials = ApiKeyCredentials(in_headers={"Prediction-key": cv_key})
@@ -26,14 +23,13 @@ def predict(image_path):
 
     # Get the images and show the predicted classes for each one
 
-
     # Open the image, and use the custom vision model to classify it
     image_contents = open(image_path, 'rb')
     classification = custom_vision_client.classify_image(project_id, model_name, image_contents.read())
 
     results = []
     for i in range(len(classification.predictions)):
-        results.append([classification.predictions[i].tag_name, round(classification.predictions[i].probability*100,2)])
+        results.append(
+            [classification.predictions[i].tag_name, round(classification.predictions[i].probability * 100, 2)])
 
     return results
-
